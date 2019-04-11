e46c5e3862114a85f30c33bd64aedaca3eb222b5
Myles Andre <myles.andre@possible.com>
2019-04-10 20:06:01 -0400

    Created stubs for tests

--- src/GradesDB.java
+++ src/GradesDB.java
@@ -1,2 +1,40 @@
+import java.util.HashSet;
+
 public class GradesDB {
+
+    public GradesDB(String fileName) {
+
+    }
+
+
+    //Stub TODO: add logic
+    public int getNumStudents() {
+        return 0;
+    }
+
+    //Stub TODO: add logic
+    public int getNumAssignments() {
+        return 0;
+    }
+
+    //Stub TODO: add logic
+    public int getNumProjects() {
+        return 0;
+    }
+
+    //Stub TODO: add logic
+    public HashSet<Student> getStudents() {
+        return new HashSet<>();
+    }
+
+    //Stub TODO: add logic
+    public Student getStudentByName(String name) {
+        return new Student();
+    }
+
+    //Stub TODO: add logic
+    public Student getStudentByID(String id) {
+        return new Student();
+    }
+
 }
---
+++ src/Student.java
@@ -0,0 +1,22 @@
+
+//Stub
+public class Student {
+
+    //Stub TODO: add logic
+    public String getName() {
+        return "";
+    }
+
+    //Stub TODO: add logic
+    public int getAttendance() {
+        return 0;
+    }
+
+    //Stub TODO: add logic
+    public String getId() {
+        return "";
+    }
+
+
+
+}
